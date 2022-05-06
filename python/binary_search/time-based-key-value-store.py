c71e7da134a973158a5afb946924fcdfcb3b383e
Pragya Agrawal <pragya.agrwal@gmail.com>
2022-05-06 20:46:58 +0530

    binary search med questions

---
+++ python/binary_search/time-based-key-value-store.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class TimeMap:
+
+    def __init__(self):
+        self.key_list = defaultdict(list) # key: String: [(value: String, timestamp: int)]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.key_list[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.key_list:
+            return ""
+        res=""
+        values = self.key_list[key]
+        l,r = 0, len(values)-1
+        
+        while l <= r:
+            m = (l+r)//2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l=m+1
+            else:
+                r=m-1
+        return res
+                
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
